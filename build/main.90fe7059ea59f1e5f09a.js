1a2c3cb68241d64214be954c95ad9a86524779ed
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-02-13 21:30:01 +0500

    webpack until react

--- build/main.90fe7059ea59f1e5f09a.js
+++ build/main.90fe7059ea59f1e5f09a.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.js":
+/***/ "./src/index.ts":
 /*!**********************!*\
-  !*** ./src/index.js ***!
+  !*** ./src/index.ts ***!
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"Hellowo dasdssssd\");\r\n\n\n//# sourceURL=webpack://redux-and-graphql/./src/index.js?");
+eval("function sum(a, b) {\n    return a + b;\n}\nconsole.log(sum(12, 28));\n\n\n//# sourceURL=webpack://redux-and-graphql/./src/index.ts?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("console.log(\"Hellowo dasdssssd\");\r\n\n\n//# sourceURL=webpack://redux-a
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.ts"]();
 /******/ 	
 /******/ })()
 ;
---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import  path from 'path';
+import  webpack from 'webpack';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+// import type { Configuration } from "webpack";
+import  HtmlWebpackPlugin from'html-webpack-plugin';
+
+
+type Mode = 'production' | 'development';
+
+interface EnvVariables{
+  mode:Mode
+}
+
+export  default  (env:EnvVariables) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      port:3000,
+      open:true
+    },
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        filename: 'index.html',
+        template:path.resolve(__dirname, 'public', 'index.html')
+    })],
+}
+    return config
+
+};
