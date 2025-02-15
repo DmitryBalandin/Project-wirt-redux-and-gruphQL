b6d4dda6d16f2eb0ae5af62dacfa1de821794fea
DmitryBalandin <balandinqwerty1983@gmail.com>
2025-02-15 19:41:49 +0500

    until refactoring config

---
+++ config/buildResolver.ts
@@ -0,0 +1,15 @@
+import  webpack  from "webpack"
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import path from "path";
+import { BuildOptions } from "./types/types";
+
+
+
+
+export function buildWebpack(options:BuildOptions): webpack.Configuration {
+    
+    return {
+        
+
+    }
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,49 +1,64 @@
-import  path from 'path';
-import  webpack from 'webpack';
+import path from 'path';
+import webpack from 'webpack';
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-// import type { Configuration } from "webpack";
-import  HtmlWebpackPlugin from'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 
 type Mode = 'production' | 'development';
 
-interface EnvVariables{
-  mode:Mode
+interface EnvVariables {
+  mode: Mode
 }
 
-export  default  (env:EnvVariables) => {
+export default (env: EnvVariables) => {
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     devtool: 'inline-source-map',
     devServer: {
       static: './dist',
-      port:3000,
-      open:true
+      port: 3000,
+      open: true
     },
     module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-      },
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            env.mode !== "production" ?
+              "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"],
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true
     },
     plugins: [new HtmlWebpackPlugin({
-        title: 'My App',
-        filename: 'index.html',
-        template:path.resolve(__dirname, 'public', 'index.html')
-    })],
-}
-    return config
+      title: 'My App',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    ],
+
+  }
+  return config
 
 };
